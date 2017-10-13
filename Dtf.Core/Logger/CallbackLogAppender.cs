ac3060eaa24df0a4c869663462d67e67131e0ef4
AllenLius <Guoh.liu@gmail.com>
2017-10-13 18:01:53 +0800

    add new appender.

---
+++ Dtf.Core/Logger/CallbackLogAppender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Dtf.Core;
+
+namespace Dtf.Core
+{
+    public class CallbackLogAppender : ILogAppender
+    {
+        private Action<LogLevel, string> m_callback;
+
+        public CallbackLogAppender(Action<LogLevel, string> callback)
+        {
+            m_callback = callback;
+        }
+
+        public void LogLine(LogLevel logLevel, string message)
+        {
+            m_callback(logLevel, message);
+        }
+    }
+}
